b5ee791d01512af41159d375f50763fa92ed15f4
kotala <kotarolab@gmail.com>
2018-04-04 08:40:00 +0900

    P103 connect auction

--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -3,7 +3,7 @@ import auctionsniper.ui.Main;
 public class ApplicationRunner {
 	public static final String SNIPER_ID = "sniper";
 	public static final String SNIPER_PASSWORD = "sniper";
-	public static final String XMPP_HOSTNAME = "localhost:9090";
+	public static final String XMPP_HOSTNAME = "localhost";
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST = "lost";
 	private AuctionSniperDriver driver;
--- src/auctionsniper/ui/Main.java
+++ src/auctionsniper/ui/Main.java
@@ -7,10 +7,25 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.junit.experimental.categories.Categories;
+
 public class Main {
+	private static final int ARG_HOSTNAME = 0;
+	private static final int ARG_USERNAME = 1;
+	private static final int ARG_PASSWORD = 2;
+	private static final int ARG_ITEM_ID = 3;
 	private MainWindow ui;
+	
 	public static final String MAIN_WINDOW_NAME = "Action Sniper Main";
 	public static final String SNIPER_STATUS_NAME = "sniper status";
+	public static final String AUCTION_RESOURCE = "Auction";
+	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+	public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 	
 	public Main() throws Exception {
 		startUserInterface();
@@ -18,6 +33,28 @@ public class Main {
 	
 	public static void main(String... args) throws Exception {
 		Main main = new Main();
+		XMPPConnection connection = connectTo(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
+		
+		Chat chat = connection.getChatManager().createChat(auctionId(args[ARG_ITEM_ID], connection), new MessageListener() {
+			
+			@Override
+			public void processMessage(Chat aChat, Message message) {
+				// TODO Auto-generated method stub
+			}
+		});
+		chat.sendMessage(new Message());
+	}
+	
+	private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+		XMPPConnection connection = new XMPPConnection(hostname);
+		connection.connect();
+		connection.login(username, password, AUCTION_RESOURCE);
+		
+		return connection;
+	}
+	
+	private static String auctionId(String itemId, XMPPConnection connection) {
+		return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
 	}
 	
 	private void startUserInterface() throws Exception {
