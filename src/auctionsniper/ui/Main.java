889fca234762e3d1ce2c478d98acf098d9f945c1
kotala <kotarolab@gmail.com>
2018-04-04 08:13:38 +0900

    P102 test failure

--- src/auctionsniper/ui/Main.java
+++ src/auctionsniper/ui/Main.java
@@ -1,7 +1,11 @@
 package auctionsniper.ui;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.LineBorder;
 
 public class Main {
 	private MainWindow ui;
@@ -25,11 +29,23 @@ public class Main {
 	}
 	
 	public class MainWindow extends JFrame {
+		public static final String SNIPER_STATUS_NAME = "sniper status";
+		public static final String STATUS_JOINING = "Joining";
+		private final JLabel snipetStatis = createLabel(STATUS_JOINING);
+		
 		public MainWindow() {
 			super("Auction Sniper");
 			setName(MAIN_WINDOW_NAME);
+			add(snipetStatis);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setVisible(true);
+		}
+		
+		private JLabel createLabel(String initialText) {
+			JLabel result = new JLabel(initialText);
+			result.setName(SNIPER_STATUS_NAME);
+			result.setBorder(new LineBorder(Color.BLACK));
+			return result;
 		}
 	}
 }
