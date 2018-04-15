48cb34c6614bd0f66f1fbf57c0a37f39a795daf1
kotala <kotarolab@gmail.com>
2018-04-15 14:46:26 +0900

    Add SniperState

---
+++ src/auctionsniper/SniperState.java
@@ -0,0 +1,24 @@
+package auctionsniper;
+
+import com.objogate.exception.Defect;
+
+public enum SniperState {
+	JOINNING {
+		@Override
+		public SniperState whenAuctionClosed() { return LOST; }
+	},
+	BIDDING {
+		@Override
+		public SniperState whenAuctionClosed() { return LOST; }
+	},
+	WINNING {
+		@Override
+		public SniperState whenAuctionClosed() { return WON; }
+	},
+	LOST,
+	WON;
+
+	public SniperState whenAuctionClosed() {
+		throw new Defect("Auction is already closed");
+	}
+}
